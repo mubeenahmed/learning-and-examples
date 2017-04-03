0dc514fbb35422b8bf84166c260510d43058caed
Mubeen Ahmed <Mubeen Ahmed>
2017-04-04 00:04:14 +0500

    learning http

--- bin/example_clj/io_http.clj
+++ bin/example_clj/io_http.clj
@@ -1,2 +1,17 @@
 (ns example-clj.io-http)
 
+
+;; fire and forget, returns immediately [1], return promise is ignored
+(http/get "https://domain.com/api")
+
+(def option {:timeout 200
+             :basic-auth ["user" "pass"]
+             :query-params {:param1 "value" :param2 "value"}
+             :headers {"X-Header" "Value"}
+             })
+
+(http/get "https://domain.com/api" option
+          (fn [{:keys [status headers body errors]}]
+            (if error
+              (println "Failed " error)
+              (println "OK" status ))))
