b6831b1ebc1cb15a3894bd3083d33488b2adca6b
Mubeen Ahmed <Mubeen Ahmed>
2017-04-03 23:31:44 +0500

    Learning file io operation

--- bin/example_clj/collections.clj
+++ bin/example_clj/collections.clj
@@ -1,6 +1,48 @@
 (ns example-clj.collections)
 
-; Clojure Collection
-; List
-; Vector
-; Hash
+;; List
+;; This is the simple link list implemented. 
+;; Therefore complexity is for simple link list
+(println '(1 2 3 4))
+;; OR
+(println (list 1 2 3 4))
+
+;; Vector
+;; This is the red black search binary three. 
+;; It is the recommended way to store data
+(println [1 2 3 4])
+(println [:a :b :C :d])
+
+;; Hash Maps and Sorted Maps;
+;; These are the datastructures with the key and values
+(println (hash-map :key1 1 :key2 2))
+(println {:key1 1 :key2 2})
+(println (sorted-map :key2 1 :key1 2))
+
+
+;; Set
+;; These are the distinct values
+;; It works on every data structure
+(println (set [1 2 3 4 4 5 2]))
+(println (set {:one 1 :two 2 :three 3}))
+(println (set "abcd"))
+(println '(1 2 3 4 3))
+
+;;
+(println (map #(* %1 2) [1 2 3] ) )
+(println (map #(* %1 2) '(1 2 3) ) )
+
+
+(println (reduce + [1 2 3]))
+
+
+;; Conj, adds new value to the existing data structure and return new data structure
+(println (conj [1 2 3] 4))
+(println (conj '(1 2 3) 4))
+(println (conj [[1 2] [3 4]] [5 6]))
+; conjoining with map only takes vector with exactly 2 data
+(println (conj {1 2 3 4} [1 2]))
+(println (conj {:firstname "John" :lastname "Doe"} {:age 25 :nationality "Chinese"}))
+
+(println (flatten [1 [1 2 3]]))
+(println (flatten '(1 2 [3 (4 5)])))
--- src/example_clj/io_file_system.clj
+++ src/example_clj/io_file_system.clj
@@ -1,2 +1,24 @@
 (ns example-clj.io-file-system)
 
+;; Clojure can use java file io
+;; Slurp can be used directly on value io/resource
+
+(def file "C:\\Users\\NLN\\example-clj\\src\\example_clj\\my-data.txt")
+
+;; To read an entire file to memory
+(def fetch-my-data (slurp file))
+(println fetch-my-data)
+
+
+;; To read the file line by line
+;; We can use java io
+(defn read-line-by-line []
+  (with-open [rdr (clojure.java.io/reader "C:\\Users\\NLN\\example-clj\\src\\example_clj\\my-data.txt")]
+    (reduce conj [] (line-seq rdr))))
+(println (read-line-by-line))
+
+
+(defn write-line-by-line []
+  (with-open [wtr (clojure.java.io/writer "C:\\Users\\NLN\\example-clj\\src\\example_clj\\my-data.txt" :append true)]
+    (.write wtr (str "\n" "this line is new"))))
+(write-line-by-line)
