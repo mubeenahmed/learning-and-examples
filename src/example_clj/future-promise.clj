66f60eceb604a2baeb084fe3b0ba8ee8ec6a7d56
Mubeen Ahmed <mubeenahmedsiddiqui@gmail.com>
2017-04-18 11:13:50 +0500

    Update future-promise.clj

--- src/example_clj/future-promise.clj
+++ src/example_clj/future-promise.clj
@@ -9,3 +9,19 @@
 (println "another process .. ")
 (println "factorial process completed" @f)
 (println "done!")
+
+
+;; Promise, is also the asynchoronus operation. But until the promise has not finished with operation
+;; and delivered is performed then it will block the thread
+;; Promise are delivered once, and once delivered they cannot be reuse.
+;; Promise encapsulate the immutable data
+;; Promise is thread-safe and its value cannot be changed once delivered or set
+(defn long-running-op []
+  (do (Thread/sleep 200) 20))
+
+(def answer (promise))
+(deliver answer (long-running-op))
+
+(println "some other process")
+(println "long running operation ends with " @answer)
+(println "done!")
