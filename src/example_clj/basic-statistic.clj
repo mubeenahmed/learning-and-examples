b7a6ecbf4158f29add0c778c5f19c391f0b9fd26
Mubeen Ahmed <mubeenahmedsiddiqui@gmail.com>
2017-05-18 10:36:22 +0500

    Adding Mean, Variance, and Standard Deviations

--- src/example_clj/basic-statistic.clj
+++ src/example_clj/basic-statistic.clj
@@ -3,11 +3,14 @@
 ;; Some sample data
 (def heights [600 470 170 430 300] )
 
+;; Need the count in multiple places for calculating means and variance
+(def c (count heights))
+
 ;; Calculating the means
-(def means (/ (reduce + heights) (count heights) ))
+(def means (/ (reduce + heights) c ))
 
 ;; Calculating the variance by Sum of ( Mean - X ) and powering by 2.s 
-(def variance (/ (reduce + (map #(Math/pow (- means %) 2) heights)) (count heights)) )
+(def variance (/ (reduce + (map #(Math/pow (- means %) 2) heights)) c) )
 
 ;; Calcuating the standard deviation by square root
 (def standard-deviation (Math/sqrt variance) )
